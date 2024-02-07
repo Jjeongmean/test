507b2d582da72d180c52623e05ac7304e339a922
Jjeongmean <exo1461@naver.com>
2024-02-07 11:21:06 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//public class Main {
+//    public static void main(String[] args) {
+//      System.out.println(("안녕 자바!"));
+//      System.out.println(("헬로 자바!"));
+//      //줄바꿈 사용 시 println 사용
+//
+//        System.out.print("안녕 자바!");
+//       System.out.print("헬로 자바");
+//       //줄바꿈 사용 안할시 print 사용
+//
+//        //sout ➡ System.out.printIn 단축키
+//        /*
+//        여러줄 주석
+//         */
+//        System.out.printIn("헬로 자바");
+//    }
+//}
